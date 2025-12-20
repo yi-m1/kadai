46daef6be5952fc7f3a6a614fa0e853753525c07
maeda-n12345 <smtrknnk.1626@gmail.com>
2025-12-20 14:11:46 +0900

    ViewResolverを削除した

--- src/main/java/jp/co/sfrontier/ss3/game/config/WebConfig.java
+++ src/main/java/jp/co/sfrontier/ss3/game/config/WebConfig.java
@@ -1,11 +1,9 @@
 package jp.co.sfrontier.ss3.game.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  * Spring Web MVC の初期化を行う
@@ -13,18 +11,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 // すべてのコンポーネントをスキャンしたいわけではないので、明示的にスキャンする
-@ComponentScan(basePackages = { "jp.co.sfrontier.ss3.game.controller" ,"jp.co.sfrontier.ss3.game.service"})
+@ComponentScan(basePackages = { "jp.co.sfrontier.ss3.game.controller", "jp.co.sfrontier.ss3.game.service" })
 public class WebConfig implements WebMvcConfigurer {
-	
-	/**
-	 * JSP ファイルのマッピング定義をする
-	 * @return
-	 */
-	@Bean
-	protected InternalResourceViewResolver internalResourceViewResolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/jsp/");
-		resolver.setSuffix(".jsp");
-		return resolver;
-	}
+
 }
