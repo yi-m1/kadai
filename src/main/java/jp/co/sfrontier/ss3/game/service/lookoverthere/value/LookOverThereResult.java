b329b7b112526567e67abeea2673756ab352d626
sf0549 <sf0549@SF138054>
2026-01-21 00:05:21 +0900

    コメントを追記した

--- src/main/java/jp/co/sfrontier/ss3/game/service/lookoverthere/value/LookOverThereResult.java
+++ src/main/java/jp/co/sfrontier/ss3/game/service/lookoverthere/value/LookOverThereResult.java
@@ -2,22 +2,17 @@ package jp.co.sfrontier.ss3.game.service.lookoverthere.value;
 
 import jp.co.sfrontier.ss3.game.common.Direction;
 import jp.co.sfrontier.ss3.game.common.ResultCode;
+import lombok.Value;
 
+/**
+ * 1回のプレイ結果を Controller に返す Value <br>
+ * <br>
+ * private final フィールド、コンストラクタ、getter は自動生成される
+ */
+@Value
 public class LookOverThereResult {
 
-    private final ResultCode resultCode;
-    private final Direction defenderDirection;
+	ResultCode resultCode;
+	Direction defenderDirection;
 
-    public LookOverThereResult(ResultCode resultCode, Direction defenderDirection) {
-        this.resultCode = resultCode;
-        this.defenderDirection = defenderDirection;
-    }
-
-    public ResultCode getResultCode() {
-        return resultCode;
-    }
-
-    public Direction getDefenderDirection() {
-        return defenderDirection;
-    }
 }
