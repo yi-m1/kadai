8e621e866c11b04ed692c435c5f02129f17cb461
sf0558 <sf0558@SF139042>
2026-01-27 12:27:18 +0900

    develop マージ後の履歴取得処理を修正

--- src/main/java/jp/co/sfrontier/ss3/game/controller/HistoryController.java
+++ src/main/java/jp/co/sfrontier/ss3/game/controller/HistoryController.java
@@ -1,4 +1,4 @@
-package jp.co.sfrontier.ss3.game.controller;
+/*package jp.co.sfrontier.ss3.game.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,31 +8,32 @@ import jp.co.sfrontier.ss3.game.service.lookoverthere.LookOverTherePlayService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-/**
- * 対戦履歴画面の表示を制御するクラス<br>
- * <br>
- */
+*//**
+	* 対戦履歴画面の表示を制御するクラス<br>
+	* <br>
+	*/
+/*
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HistoryController {
-	private final LookOverTherePlayService playService;
+private final LookOverTherePlayService playService;
 
-	/**
-	 * 対戦履歴を表示する<br>
-	 * <br>
-	 * @param model 対戦履歴画面に渡すモデル
-	 * @return 対戦履歴画面
-	 */
-	@GetMapping("/history")
-	public String history(Model model) {
+*//**
+	* 対戦履歴を表示する<br>
+	* <br>
+	* @param model 対戦履歴画面に渡すモデル
+	* @return 対戦履歴画面
+	*//*
+		@GetMapping("/history")
+		public String history(Model model) {
 		// ログイン未実装のため固定 ID
 		Long playerId = 1L;
-
+		
 		log.info("対戦履歴表示 playerId={}", playerId);
-
+		
 		model.addAttribute("histories", playService.getHistory(playerId));
-
+		
 		return "history";
-	}
-}
+		}
+		}*/
