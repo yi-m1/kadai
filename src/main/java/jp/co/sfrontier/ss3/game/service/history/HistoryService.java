388a113cc9823be254586a01ff7c9044722694cc
Reo0414 <146312453+Reo0414@users.noreply.github.com>
2026-01-30 12:00:59 +0900

    Refactor HistoryService for improved readability

--- src/main/java/jp/co/sfrontier/ss3/game/service/history/HistoryService.java
+++ src/main/java/jp/co/sfrontier/ss3/game/service/history/HistoryService.java
@@ -19,51 +19,51 @@ import jp.co.sfrontier.ss3.game.repository.ResultHistoryDao;
  */
 public class HistoryService {
 
-    private static final Logger logger
-            = LogManager.getLogger(HistoryService.class);
+	private static final Logger logger
+			= LogManager.getLogger(HistoryService.class);
 
-    /**
-     * 対戦履歴を取得する。<br>
-     * <br>
-     * 該当ユーザーの全履歴を取り出す。<br>
-     * <br>
-     * @param userId このユーザーの履歴を取得する
-     * @return 履歴一覧を降順で返す。該当データが存在しない場合空のリストを返す。
-     * @throws SQLException DBへの接続エラー等が発生した場合。
-     */
-    public List<ResultHistory> getHistory(int userId) throws SQLException {
+	/**
+	 * 対戦履歴を取得する。<br>
+	 * <br>
+	 * 該当ユーザーの全履歴を取り出す。<br>
+	 * <br>
+	 * @param userId このユーザーの履歴を取得する
+	 * @return 履歴一覧を降順で返す。該当データが存在しない場合空のリストを返す。
+	 * @throws SQLException DBへの接続エラー等が発生した場合。
+	 */
+	public List<ResultHistory> getHistory(int userId) throws SQLException {
 
-        // DBから履歴情報を取得する。
-        // 1. DBに繋ぐ
-        // 2. DBからデータを検索してくる。
-        // 3. DBから切断する。
-        Connection conn = DbUtil.getConnection();
-        ResultHistoryDao resultHistoryDao = new ResultHistoryDao(conn);
-        try {
-            List<ResultHistory> list
-                    = resultHistoryDao.getResultHistoryInfo(userId);
-            DbUtil.commit(conn);
+		// DBから履歴情報を取得する。
+		// 1. DBに繋ぐ
+		// 2. DBからデータを検索してくる。
+		// 3. DBから切断する。
+		Connection conn = DbUtil.getConnection();
+		ResultHistoryDao resultHistoryDao = new ResultHistoryDao(conn);
+		try {
+			List<ResultHistory> list
+					= resultHistoryDao.getResultHistoryInfo(userId);
+			DbUtil.commit(conn);
 
-            return list;
-        } catch (SQLException e) {
-            logger.error("履歴の検索に失敗しました。[userId={}]", userId, e);
-            rollback(conn);
-            throw e;
-        } finally {
-            DbUtil.close(conn);
-        }
-    }
+			return list;
+		} catch (SQLException e) {
+			logger.error("履歴の検索に失敗しました。[userId={}]", userId, e);
+			rollback(conn);
+			throw e;
+		} finally {
+			DbUtil.close(conn);
+		}
+	}
 
-    /**
-     * ロールバックする際の例外をスローさせないために作った
-     * @param conn
-     */
-    private void rollback(Connection conn) {
-        try {
-            DbUtil.rollback(conn);
-        } catch (SQLException e) {
-            logger.error("ロールバックに失敗しました", e);
-        }
-    }
+	/**
+	 * ロールバックする際の例外をスローさせないために作った
+	 * @param conn
+	 */
+	private void rollback(Connection conn) {
+		try {
+			DbUtil.rollback(conn);
+		} catch (SQLException e) {
+			logger.error("ロールバックに失敗しました", e);
+		}
+	}
 
 }
